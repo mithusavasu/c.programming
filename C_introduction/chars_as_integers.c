4e298143113768594780cf7f6bed129add746b11
Mithusa Vasu <mvasudevan@torontomu.ca>
2025-11-16 01:01:52 +0000

    chars_as_integers

---
+++ C_introduction/chars_as_integers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    int num1 = 84;
+    char c1 = 'A';
+    printf("c1 as int = %d\n", c1);
+    printf("num1 as char = %c\n", num1);
+    return(0);
+    
+}
